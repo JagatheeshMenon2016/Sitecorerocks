28edd16f019efe560065547b2c348f04563a9812
Jagatheesh Menon <jack3394@gmail.com>
2016-11-24 10:20:42 +0000

    Change done on Nov 24/2016

---
+++ SourceCode/SitecoreRocks/SitecoreRocks/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreRocks.Models
+{
+    public class Product :Sitecore.Mvc.Presentation.RenderingModel
+    {
+        public HtmlString Title { get { return new HtmlString(FieldRenderer.Render(Item, "title")); } }
+        public HtmlString Description { get { return new HtmlString(FieldRenderer.Render(Item, "description")); } }
+        public HtmlString Image { get { return new HtmlString(FieldRenderer.Render(Item, "Image")); } }
+        public HtmlString Price { get { return new HtmlString(FieldRenderer.Render(Item, "price")); } }
+
+    }
+}
